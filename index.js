80b0d21e5706ca9750547178595b345f3810d6f3
AotherQ <ahmetumutdogan159@gmail.com>
2023-06-21 00:19:24 +0300

    My Portfolio Site

--- index.js
+++ index.js
@@ -1,43 +1,57 @@
-const token = 'cd6b2d7ecd214129bf548e7cf4fa1c23';
+const clientId = 'cd6b2d7ecd214129bf548e7cf4fa1c23';
+const clientSecret = '6549c8a8178c4888a87f636e95de585f';
+
+async function getToken() {
+  const res = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
+    },
+    body: 'grant_type=client_credentials',
+  });
+  const data = await res.json();
+  return data.access_token;
+}
 
 async function fetchWebApi(endpoint, method, body) {
-    const res = await fetch(`https://api.spotify.com/${endpoint}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-        method,
-        body: JSON.stringify(body)
-    });
-    return await res.json();
+  const token = await getToken();
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body: JSON.stringify(body),
+  });
+  return await res.json();
 }
 
 async function getTopTracks() {
-    return (await fetchWebApi(
-        'v1/me/top/tracks?time_range=short_term&limit=5',
-        'GET'
-    )).items;
+  return (await fetchWebApi(
+    'v1/me/top/tracks?time_range=short_term&limit=5',
+    'GET'
+  )).items;
 }
 
-// Sonuçları HTML'e ekleme
 async function showTopTracks() {
-    const topTracks = await getTopTracks();
-    const topTracksList = document.getElementById('top-tracks');
-    topTracksList.innerHTML = ''; // Şarkı listesini temizle
+  const topTracks = await getTopTracks();
+  const topTracksList = document.getElementById('top-tracks');
+  topTracksList.innerHTML = '';
 
-    topTracks.forEach(track => {
-        const li = document.createElement('li');
-        const img = document.createElement('img');
-        const span = document.createElement('span');
+  topTracks.forEach(track => {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    const span = document.createElement('span');
 
-        img.src = track.album.images[0].url; // İlk resmin URL'sini al
-        span.textContent = `${track.name} by ${track.artists.map(artist => artist.name).join(', ')}`;
+    img.src = track.album.images[0].url;
+    span.textContent = `${track.name} by ${track.artists.map(artist => artist.name).join(', ')}`;
 
-        li.appendChild(img);
-        li.appendChild(span);
-        topTracksList.appendChild(li);
-    });
+    li.appendChild(img);
+    li.appendChild(span);
+    topTracksList.appendChild(li);
+  });
 }
 
 window.addEventListener('load', () => {
-    showTopTracks();
+  showTopTracks();
 });
